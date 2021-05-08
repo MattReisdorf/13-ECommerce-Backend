292d80e919bbb172af685986f3e32f63b22dc22e
Matt Reisdorf <mattreisdorf@Matts-MacBook-Pro.local>
2021-05-08 18:04:15 -0400

    product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -61,12 +61,17 @@ router.get('/:id', async (req, res) => {
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
+  {
+	  "product_name": "Basketball",
+	  "price": 200.00,
+	  "stonk": 3,
+	  "tagIds": [1, 2, 3, 4],
+	  "category_id": 7,
+	  "category": {
+		  "id": 7,
+		  "category_name": "Sports Equipment"
+	    }
+  }
   */
   Product.create(req.body)
     .then((product) => {
@@ -93,6 +98,16 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
+
+   /* req.body should look like this...
+  {
+	  "product_name": "Basketball",
+	  "price": 200.00,
+	  "stonk": 3,
+	  "tagIds": [1, 2, 3, 4]
+  }
+  */
+ 
   Product.update(req.body, {
     where: {
       id: req.params.id,
@@ -132,8 +147,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No Product Found With This ID' })
+    }
+
+    res.status(200).json(productData);
+  }
+
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
